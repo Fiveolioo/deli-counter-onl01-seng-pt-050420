2d2be9abe8c10a9aa9cf5adc4f8f74752dc6e4d5
Richard Cho <96richardohc@gmail.com>
2020-05-12 07:18:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli == 0
+  if katz_deli.empty?
     puts "The line is currently empty."
   else line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, index|
