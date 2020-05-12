73da95dd8e428f94d81324d1064046f8c661f12b
Richard Cho <96richardohc@gmail.com>
2020-05-12 07:08:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0
+    katz_deli.each.with_index |name, index|
     
-  end
+    end
 end
 
 def take_a_number(katz_deli, name)
