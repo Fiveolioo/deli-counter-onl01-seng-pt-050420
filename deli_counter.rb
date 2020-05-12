ee000d24d3728312bf11fc9ef4281e217cf631d8
Richard Cho <96richardohc@gmail.com>
2020-05-12 07:07:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,7 @@
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  elsif
-    line = "The line is currently:"
-    katz_deli.length > 0
+  elsif katz_deli.length > 0
     
   end
 end
